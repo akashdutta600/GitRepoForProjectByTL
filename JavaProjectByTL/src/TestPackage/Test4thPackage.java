8a8b57d437afec725140c1854b509db36d0f27ad
Akash Dutta <akashdutta900@gmail.com>
2020-09-18 22:44:28 +0530

    test 1

--- JavaProjectByTL/src/TestPackage/Test4thPackage.java
+++ JavaProjectByTL/src/TestPackage/Test4thPackage.java
@@ -3,7 +3,8 @@ package TestPackage;
 public class Test4thPackage {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from 4th package..");
+	System.out.println("Hello from 4th package..");
+	public int a=2;
 
 	}
 
